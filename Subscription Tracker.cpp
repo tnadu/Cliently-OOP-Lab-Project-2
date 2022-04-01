7f4839b1005783a92ee72424f131490437c94871
Toma Nadu <toma.nadu@protonmail.com>
2022-04-01 20:08:44 +0300

    Class definitions almost completed, not yet tested

--- Subscription Tracker.cpp
+++ Subscription Tracker.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -10,10 +11,10 @@ bool isNumber (string x) {
 }
 
 class Person {
+protected:
     static int IDglobal;
     int ID;
     string name, CNP;
-protected:
     static int getIDglobal() { return IDglobal; };
 public:
     Person() : ID(IDglobal++), name("None"), CNP("0000000000000") {};
@@ -22,7 +23,7 @@ public:
 
     Person(const Person &person) : ID(IDglobal++), name(person.name), CNP(person.CNP) {};
 
-    ~Person();
+    virtual ~Person();
 
     Person &operator=(const Person &person);
 
@@ -48,10 +49,10 @@ int Person::IDglobal = 0;
 Person::Person(string name, string CNP) {
     if (!isNumber(CNP)) {
         cout<<"Error: CNP must be numerical\n";
-        return;
+        this->CNP="0000000000000";
     }
+    else this->CNP=CNP;
     this->name=name;
-    this->CNP=CNP;
     this->ID=IDglobal++;
 }
 
@@ -68,10 +69,11 @@ ostream &operator<<(ostream &out, const Person &person) {
 }
 
 istream &operator>>(istream &in, Person &person) {
-    in >> person.name >> person.CNP;
+    getline(in, person.name, '\t');
+    getline(in, person.CNP, '\t');
     while(!isNumber(person.CNP)) {
         cout<<"Error: CNP must be numerical\n";
-        in>>person.CNP;
+        getline(in, person.CNP);
     }
     return in;
 }
@@ -124,7 +126,7 @@ public:
     Subscription(const Subscription &subscription) : name(subscription.name), price(subscription.price),
                                                      period(subscription.period) {};
 
-    ~Subscription();
+    virtual ~Subscription();
 
     Subscription &operator=(const Subscription &subscription);
 
@@ -147,22 +149,22 @@ public:
 
 Subscription::Subscription(string name, float price, int period) {
     if (name!="Individual" and name!="Dual" and name!="Family") {
-        cout<<"Error: Subscription name can be 'Individual', 'Dual' or 'Family'\n";
-        return;
+        cout<<"Error: Subscription name was set to 'None', as it can only be one of the following: 'Individual', 'Dual' or 'Family'\n";
+        this->name="None";
     }
-    this->name=name;
+    else this->name=name;
 
     if (price<0) {
-        cout<<"Error: Subscription price must be a positive floating-point number\n";
-        return;
+        cout<<"Error: Subscription price was set to 0, as it must be a positive floating-point number\n";
+        this->price=0;
     }
-    this->price=price;
+    else this->price=price;
 
     if (period<0) {
-        cout<<"Error: Subscription period must be a positive integer\n";
-        return;
+        cout<<"Error: Subscription period was set to 0, as it must be a positive integer\n";
+        this->period=0;
     }
-    this->period=period;
+    else this->period=period;
 }
 
 Subscription& Subscription::operator=(const Subscription &subscription) {
@@ -178,11 +180,11 @@ ostream& operator<<(ostream& out, const Subscription& subscription) {
 }
 
 istream& operator>>(istream& in, Subscription& subscription) {
-    in>>subscription.name;
+    getline(in, subscription.name, '\t');
 
     while(subscription.name!="Individual" and subscription.name!="Dual" and subscription.name!="Family") {
         cout<<"Error: Subscription name can be 'Individual', 'Dual' or 'Family'\n>>> ";
-        in>>subscription.name;
+        getline(in, subscription.name);
     }
 
     in>>subscription.price;
@@ -204,10 +206,10 @@ istream& operator>>(istream& in, Subscription& subscription) {
 
 void Subscription::setName (string name) {
     if (name!="Individual" and name!="Dual" and name!="Family") {
-        cout<<"Error: Subscription name can be 'Individual', 'Dual' or 'Family'\n";
-        return;
+        cout<<"Error: Subscription name was set to 'None', as it can only be one of the following: 'Individual', 'Dual' or 'Family'\n";
+        this->name="None";
     }
-    this->name=name;
+    else this->name=name;
 }
 
 string Subscription::getName () const {
@@ -216,10 +218,10 @@ string Subscription::getName () const {
 
 void Subscription::setPrice (float price) {
     if (price<0) {
-        cout<<"Error: Subscription price must be a positive floating-point number\n";
-        return;
+        cout<<"Error: Subscription price was set to 0, as it must be a positive floating-point number\n";
+        this->price=0;
     }
-    this->price=price;
+    else this->price=price;
 }
 
 float Subscription::getPrice () const {
@@ -228,10 +230,10 @@ float Subscription::getPrice () const {
 
 void Subscription::setPeriod (int period) {
     if (period<0) {
-        cout<<"Error: Subscription period must be a positive integer\n";
-        return;
+        cout<<"Error: Subscription period was set to 0, as it must be a positive integer\n";
+        this->period=0;
     }
-    this->period=period;
+    else this->period=period;
 }
 
 int Subscription::getPeriod () const {
@@ -240,6 +242,7 @@ int Subscription::getPeriod () const {
 
 
 class PremiumSubscription: public Subscription {
+protected:
     int discount;
 public:
     PremiumSubscription(): Subscription(), discount(0) {};
@@ -261,12 +264,12 @@ public:
     int getDiscount() const;
 };
 
-PremiumSubscription::PremiumSubscription(string name, float price, int period, int discount) {
+PremiumSubscription::PremiumSubscription(string name, float price, int period, int discount): Subscription(name, price, period) {
     if (discount<0) {
-        cout<<"Error: Subscription discount must be a positive integer\n";
+        cout << "Error: Subscription discount was set to 0, as it must be a positive integer\n";
+        this->discount=0;
         return;
     }
-    Subscription(name, price, period);
     this->discount=discount;
 }
 
@@ -282,6 +285,7 @@ PremiumSubscription& PremiumSubscription::operator=(const PremiumSubscription &p
     price=premium.price;
     period=premium.period;
     discount=premium.discount;
+    return *this;
 }
 
 ostream& operator<<(ostream& out, const PremiumSubscription &premium) {
@@ -313,42 +317,197 @@ int PremiumSubscription::getDiscount() const {
 
 
 class Subscriber : public Person {
+protected:
     string phoneNumber;
-    Subscription x;
+    Subscription *x;
 public:
-    Subscriber() : Person(), x(), phoneNumber("0000000000") {};
+    Subscriber() : Person(), x(new Subscription), phoneNumber("0000000000") {};
 
-    Subscriber(string name, string CNP, string phoneNumber, string subscriptionName, float price, int period);
+    Subscriber(string name, string CNP, string phoneNumber, string subscriptionName, float price, int period, int discount=0);
 
     Subscriber(const Subscriber &subscriber);
 
     ~Subscriber();
 
     Subscriber& operator=(Subscriber &subscriber);
+
+    friend ostream& operator<<(ostream &out, const Subscriber &subscriber);
+
+    friend istream& operator>>(istream &in, Subscriber &subscriber);
+
+    void setPhoneNumber (string phoneNumber);
+
+    string getPhoneNumber ();
 };
 
-Subscriber::Subscriber(string name, string CNP, string phoneNumber, string subscriptionName, float price, int period) {
-    if (!isNumber(phoneNumber)) {
-        cout<<"Error: Phone number must be numerical\n";
-        return;
+Subscriber::Subscriber(string name, string CNP, string phoneNumber, string subscriptionName, float price, int period, int discount):
+Person(name, CNP)  {
+    if (discount<0) {
+        cout<<"Error: Discount was set to 0, as it must be a positive integer\n";
+        x=new PremiumSubscription(subscriptionName, price, period, 0);
     }
-    Person(name, CNP);
-    Subscription(subscriptionName, price, period);
-    this->phoneNumber=phoneNumber;
+    else if (discount==0) {
+        x=new Subscription(subscriptionName, price, period);
+    }
+    else x=new PremiumSubscription(subscriptionName, price, period, discount);
+
+    if (!isNumber(phoneNumber)) {
+        cout<<"Error: Phone number was set to default, as it must be numerical\n";
+        this->phoneNumber="0000000000";
+    }
+    else this->phoneNumber=phoneNumber;
+
 }
 
-Subscriber::Subscriber(const Subscriber &subscriber): Person(subscriber.getName(), subscriber.getCNP()),
-                                                      x(subscriber.x.getName(), subscriber.x.getPrice(), subscriber.x.getPeriod()) {
+Subscriber::Subscriber(const Subscriber &subscriber): Person(subscriber.getName(), subscriber.getCNP()) {
+    if (PremiumSubscription *construct=dynamic_cast<PremiumSubscription*>(subscriber.x)) {
+        PremiumSubscription toBeInserted = *construct;
+        x=&toBeInserted;
+    }
+    else {
+        Subscription toBeInserted= *subscriber.x;
+        x=&toBeInserted;
+    }
     phoneNumber=subscriber.phoneNumber;
 }
 
 Subscriber& Subscriber::operator=( Subscriber &subscriber) {
-    this->setName(subscriber.getName());
-    this->setCNP(subscriber.getCNP());
+    this->name=subscriber.name;
+    this->CNP=subscriber.CNP;
     this->setID();
-    x.setName(subscriber.x.getName());
+    if (PremiumSubscription *construct=dynamic_cast<PremiumSubscription*>(subscriber.x)) {
+        PremiumSubscription toBeInserted = *construct;
+        x=&toBeInserted;
+    }
+    else {
+        Subscription toBeInserted= *subscriber.x;
+        x=&toBeInserted;
+    }
+    this->phoneNumber=subscriber.phoneNumber;
+    return *this;
+}
+
+void Subscriber::setPhoneNumber (string phoneNumber) {
+    if (!isNumber(phoneNumber)) {
+        cout<<"Error: Subscriber phone number was set to default, as it must be numerical\n";
+        this->phoneNumber="0000000000";
+    }
+    else this->phoneNumber=phoneNumber;
+}
+
+ostream& operator<<(ostream &out, const Subscriber &subscriber) {
+    out<<"Subscriber:: "<<dynamic_cast<const Person&>(subscriber)<<" - Phone Number: "<<subscriber.phoneNumber;
+    out<<"\nSubscription:: "<<subscriber.x;
+    return out;
+}
+
+istream& operator>>(istream& in, Subscriber &subscriber) {
+    in>>dynamic_cast<Person&>(subscriber);
+    string command[4];
+    getline(in, command[0], '\t');
+    getline(in, command[1], '\t');
+    in>>command[2];
+    while(stof(command[2])<0) {
+        cout<<"Error: Subscription price must be a positive floating-point number\n>>> ";
+        in>>command[2];
+    }
+
+    in>>command[3];
+
+    if (stoi(command[3])==0) {
+        subscriber.x->setName(command[0]);
+        subscriber.x->setPrice(stof(command[1]));
+        subscriber.x->setPeriod(stoi(command[2]));
+    }
+    else if (stoi(command[3])<0){
+        delete subscriber.x;
+        subscriber.x=new PremiumSubscription(command[0], stof(command[1]), stoi(command[2]), 0);
+    }
+    else {
+        delete subscriber.x;
+        subscriber.x=new PremiumSubscription(command[0], stof(command[1]), stoi(command[2]), stoi(command[3]));
+    }
+
+    in>>subscriber.phoneNumber;
+    while(!isNumber(subscriber.phoneNumber)) {
+        cout<<"Error: Subscriber phone number must be numerical\n";
+        in>>subscriber.phoneNumber;
+    }
+
+    return in;
+}
+
+
+
+
+class Clients {
+    int numberOfClients=0;
+    Subscriber *subscribers;
+public:
+    Clients();
+
+    Clients(Subscriber subscribers[], int numberOfClients);
+
+    Clients(const Clients &clients);
+
+    ~Clients();
+
+    Clients& operator=(const Clients &clients);
+
+    friend ostream& operator<<(ostream &out, const Clients &clients);
+
+    friend istream& operator>>(istream &in, Clients &clients);
+
+    int getNumberOfClients () {return numberOfClients;};
+};
+
+Clients::Clients() {
+    subscribers=new Subscriber[200];
+}
+
+Clients::Clients(Subscriber subscribers[], int numberOfClients) {
+    delete[]this->subscribers;
+    this->numberOfClients=numberOfClients;
+    for (int i=0; i<numberOfClients; i++) {
+        Subscriber aux=subscribers[i];
+        this->subscribers[i]=aux;
+    }
+}
+
+Clients::Clients(const Clients &clients) {
+    delete[]subscribers;
+    numberOfClients=clients.numberOfClients;
+    for (int i=0; i<numberOfClients; i++) {
+        Subscriber aux=clients.subscribers[i];
+        subscribers[i]=aux;
+    }
+}
+
+Clients::~Clients() {
+    delete[]subscribers;
+}
+
+Clients& Clients::operator=(const Clients& clients) {
+    delete[]subscribers;
+    numberOfClients=clients.numberOfClients;
+    for (int i=0; i<numberOfClients; i++) {
+        Subscriber aux=clients.subscribers[i];
+        subscribers[i]=aux;
+    }
+    return *this;
+}
+
+ostream& operator<<(ostream &out, const Clients &clients) {
+    out<<"All stored clients:\n";
+    for(int i=0; i<clients.numberOfClients; i++) out<<clients.subscribers[i]<<'\n';
+    return out;
+}
+
+istream& operator>>(istream &in, Clients &clients) {
+    
 }
 
 int main() {
+
     return 0;
 }
